e37af99b3b19aa94f43291c66f3f78702b816c06
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-15 01:20:59 +0700

    Закончил первое задание

--- main.js
+++ main.js
@@ -5,19 +5,42 @@ const colors = [
     '#FF9800',
     '#009688',
     '#795548',
+    '#212850',
+    '#2c2c2c',
+    '#111111',
+    'salmon',
+    'aqua',
 ];
 
 const min = 0;
-const max = colors.length-1;
+const max = colors.length - 1;
 
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// console.log(randomIntegerFromInterval(min, max));
+const startBtn = document.querySelector('.start');
+const stopBtn = document.querySelector('.stop');
 
+let intervalId = null;
 
+startBtn.addEventListener('click', () => {
+    if (intervalId === null)
+        intervalId = setInterval(() => {
+            {
+                const currentColor =
+                colors[randomIntegerFromInterval(min, max)];
+                console.log(randomIntegerFromInterval(min, max));
+                console.log(currentColor);
+                changeBackground(currentColor);
+            }
+        }, 1000);
+});
 
+function changeBackground(color) {
+    document.body.style.background = color;
+}
 
-
-
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+});
