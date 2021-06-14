52c1115dc9a259a3ea85b30ee4f312fa342a5ad3
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-06-15 00:09:40 +0700

    const colors

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-import from colors.js
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
 
-console.log(colors);
+const min = 0;
+const max = colors.length-1;
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// console.log(randomIntegerFromInterval(min, max));
+
+
+
+
+
+
